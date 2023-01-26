d692a34d799dafd613dff543fb11e7210be56605
taeju-moon <pp2lycee@naver.com>
2023-01-26 11:15:35 +0900

    build

---
+++ build/precache-manifest.d0005fd0747ad86fcefd48cf005ad316.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab5855d9bfc020aeeb8e",
+    "url": "/static/css/main.ca526424.chunk.css"
+  },
+  {
+    "revision": "ab5855d9bfc020aeeb8e",
+    "url": "/static/js/main.ead05c7a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8f0afe003d84c7adaf0b",
+    "url": "/static/css/2.b4fbb069.chunk.css"
+  },
+  {
+    "revision": "8f0afe003d84c7adaf0b",
+    "url": "/static/js/2.bdc3a648.chunk.js"
+  },
+  {
+    "revision": "41d241a50c1439301fe119c2021ecf5f",
+    "url": "/static/media/SKKUD_green.41d241a5.jpeg"
+  },
+  {
+    "revision": "ea264e51b368ff4e433741937165f606",
+    "url": "/static/media/memberImg.ea264e51.png"
+  },
+  {
+    "revision": "313c9faa283f64ddc420de0f61801c04",
+    "url": "/static/media/SKKUD_LOGO.313c9faa.png"
+  },
+  {
+    "revision": "88404504612c9db2081a0bef69b5f44c",
+    "url": "/static/media/IntroDesign.88404504.png"
+  },
+  {
+    "revision": "3936c7dba20ee0cdfa5c4c9dfdfecf7f",
+    "url": "/static/media/Art+WeareSKKUD_gray.3936c7db.jpeg"
+  },
+  {
+    "revision": "5043a62eeaa47151a54cfbc3387c6cf8",
+    "url": "/static/media/four_square.5043a62e.png"
+  },
+  {
+    "revision": "066cdcf6e3685e667f21bb6816e93203",
+    "url": "/static/media/login_default.066cdcf6.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "30969842d55bcfa5f21af82e4bf70571",
+    "url": "/index.html"
+  }
+];
