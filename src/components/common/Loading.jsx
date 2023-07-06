5e79213d29bdff88c38bff17848f91473fb6ee06
Haeun Oh <73781124+OHaEuN@users.noreply.github.com>
2023-07-06 19:21:57 +0900

    add loading spinner - member tab

--- src/components/common/Loading.jsx
+++ src/components/common/Loading.jsx
@@ -3,12 +3,8 @@ import styled from '@emotion/styled';
 import Spinner from '../../assets/loading.gif';
 
 export const Background = styled.div`
-    position: absolute;
-    width: 100vw;
-    height: 100vh;
-    top: 0;
-    left: 0;
-    background: #ffffffb7;
+    width: 100%;
+    margin-top: 80px;
     z-index: 999;
     display: flex;
     flex-direction: column;
@@ -17,6 +13,7 @@ export const Background = styled.div`
 `;
 
 export const LoadingText = styled.div`
+    margin-top: 5px;
     text-align: center;
 `;
 
@@ -24,7 +21,7 @@ const Loading = () => {
     return (
         <Background>
             <img src={Spinner} alt="로딩중" width="5%" />
-            <LoadingText>잠시만 기다려 주세요.</LoadingText>
+            <LoadingText>Loading...</LoadingText>
         </Background>
     );
 };
